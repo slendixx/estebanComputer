66c0811ff97ca0bc825c9d46ed9ff77674304f1b
Esteban Duran <esteban321duran@gmail.com>
2020-12-06 12:44:46 -0300

    starting to implement the different parsing cases

--- tools/EC0 Assembler/script.js
+++ tools/EC0 Assembler/script.js
@@ -15,51 +15,117 @@ function parse(source) {
     if (line === "" || line === "\n") continue;
     if (line.includes("//")) {
       [line, right] = line.split("//");
-      tokens = [line.split(/\s+/)];
-      tokensRight = [right.split(/\s+/)];
+      tokens = [...line.split(/\s+/)];
+      tokensRight = [...right.split(/\s+/)];
+      console.log(tokens);
+      console.log(tokens.length);
+      console.log(tokensRight);
+      console.log(tokensRight.length);
       determineCase(tokens);
       determineCase(tokensRight);
     } else {
-      tokens = [line.split(/\s+/)];
+      tokens = [...line.split(/\s+/)];
+      console.log(tokens);
+      console.log(tokens.length);
       determineCase(tokens);
     }
     //Determine with what type of line we're dealing with
   }
 }
 function determineCase(tokens) {
-  if (tokens.length === 2 && tokenIsDirective(tokens[0])) {
-    //.org arg
-  } else if (tokens.length === 2 && !tokenIsDirective(tokens[0])) {
-    //space inst
-  }else if (tokens.length === 3 && !tokenIsDirective(tokens[1])) {
-    //.org arg space
+  if (isBeg(tokens.length === 1 && tokens[0])) {
+    console.log(".beg");
+  } else if (tokens.length === 1 && isEnd(tokens[0])) {
+    console.log(".end");
+  } else if (tokens.length === 2 && isBeg(tokens[0])) {
+    console.log(".beg \\s");
+  } else if (tokens.length === 2 && isEnd(tokens[0])) {
+    console.log(".end \\s");
+  } else if (tokens.length === 2 && isOrg(tokens[0])) {
+    console.log(".org arg");
+  } else if (tokens.length === 2 && isNoArgInstruction(tokens[1])) {
+    console.log("\\s noArgInst");
+  } else if (tokens.length === 2 && isConstant(tokens[1])) {
+    console.log("\\s const");
   }
+  //Gotta come up with unique conditions for these cases
+  else if (tokens.length === 3) {
+    console.log("\\s const \\s");
+  } else if (tokens.length === 3) {
+    console.log("\\s label: const");
+  } else if (tokens.length === 3) {
+    console.log(".org arg \\s");
+  } else if (tokens.length === 3) {
+    console.log("\\s noArgInst \\s");
+  } else if (tokens.length === 3) {
+    console.log("\\s singleArgInst arg");
+  } else if (tokens.length === 4) {
+    console.log("\\s label: const \\s");
+  } else if (tokens.length === 4) {
+    console.log("\\s singleArgInst arg \\s");
+  } else if (tokens.length === 4) {
+    console.log("\\s doubleArgInst arg1 arg2");
+  } else if (tokens.length === 4) {
+    console.log("\\s label: singleArgInst arg");
+  } else if (tokens.length === 5) {
+    console.log("\\s doubleArgInst arg1 arg2 \\s");
+  } else if (tokens.length === 5) {
+    console.log("\\s label: singleArgInst arg \\s");
+  } else if (tokens.length === 5) {
+    console.log("\\s label: doubleArgInst arg1 arg2");
+  } else if (tokens.length === 6) {
+    console.log("\\s label: doubleArgInst arg1 arg2 \\s");
+  }
+  //Depending on these 20 cases, a diferent parsing and translation process is applied
 }
-function tokenIsDirective(token) {
-  if (token === ".org") return true;
-  else return false;
+function isBeg(token) {
+  return token === ".beg" ? true : false;
+}
+function isEnd(token) {
+  return token === ".end" ? true : false;
+}
+function isOrg(token) {
+  return token === ".org" ? true : false;
+}
+function isNoArgInstruction(token) {
+  switch (token) {
+    case "add":
+    case "sub":
+    case "or":
+    case "and":
+    case "not":
+      return true;
+    default:
+      return false;
+  }
 }
 function isSingleArgInstruction(token) {
   switch (token) {
     case "st":
-      break;
     case "call":
-      break;
     case "jump":
-      break;
     case "ba":
-      break;
     case "be":
-      break;
     case "bneg":
-      break;
     case "bo":
-      break;
     case "bc":
-      break;
-
+      return true;
+    default:
+      return false;
     //Add more single argument instructions when implemented here...
   }
+}
+function isDoubleArgInstruction(token) {
+  switch (token) {
+    case "ld":
+    case "ldc":
+      return true;
+    default:
+      return false;
+  }
+}
+function isConstant(token) {
+  return /^\d+$/.test(token);
 }
 function addSymbol(table, { id = "null", size = -1, address = -1 }) {
   table.push({
